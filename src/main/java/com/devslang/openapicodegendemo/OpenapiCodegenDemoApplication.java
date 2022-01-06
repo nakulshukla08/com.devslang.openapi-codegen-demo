ac99767148bca76a793df7e36bb60a72b4f2aedb
nakulshukla08 <nakulshukla08@email.com>
2022-01-06 20:27:39 +0530

    feat: demo for openapi codegen

---
+++ src/main/java/com/devslang/openapicodegendemo/OpenapiCodegenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.devslang.openapicodegendemo;
+
+import org.springframework.boot.SpringApplication;
+
+// @SpringBootApplication
+public class OpenapiCodegenDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(OpenapiCodegenDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devslang/openapicodegendemo/OpenapiCodegenDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.devslang.openapicodegendemo;
+
+import com.devslang.handler.OpenAPI2SpringBoot;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = OpenAPI2SpringBoot.class)
+class OpenapiCodegenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
